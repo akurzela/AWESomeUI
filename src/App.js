dd1fe01c5b2f494696d26694a371f070814ecfd4
Adrianna Kurzela <akurzela@amazon.ch>
2023-04-24 18:18:03 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,32 @@
-import { Button, Flex, Heading, Image, Text } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import data from "./data.json";
+import brain from './image-1601032321sjM.png';
 import './App.css';
-import { TextField } from '@aws-amplify/ui-react';
+import { useState } from 'react';
+import '@aws-amplify/ui-react/styles.css';
+import {View} from '@aws-amplify/ui-react';
 
 function App() {
+  const [fact, setFact] = useState('');
+  
+  let getFact = function() {
+   fetch("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en"
+    ).then((response) => response.json()
+    ).then((response) => { setFact(response.text)  })
+};
+
+  
   return (
-    <>
-      <header>
-        <h1>Build, Style, Launch: <font color="orange">React</font> & <font color="orange">AWS Amplify</font></h1>
-      </header>
-      <div className="content">
-        <main>
-          <div className="form">
-            <TextField label="Title" padding="1rem" width="100%" />
-            <TextField label="Description" padding="1rem" width="100%" />
-            <TextField label="Img Url" padding="1rem" width="100%" />
-          </div>
-          <Button
-            variation="primary"
-            marginBottom='1rem'
-            onClick={() => alert('Added an item!')}
-          >
-            Submit
-          </Button>
-          <Flex direction="row"
-            justifyContent="center"
-            alignItems="center"
-            alignContent="center"
-            wrap="wrap"
-            gap="1rem"
-            className='main-flex'>
-            <></>
-            {data.map((item) => {
-              return (
-                <Flex
-                  direction="row"
-                  alignItems="flex-end"
-                  wrap="wrap"
-                  gap="1rem"
-                  padding="2rem"
-                >
-                  <Image
-                    alt={item.description}
-                    height="20rem"
-                    width="15rem"
-                    src={item.filePath}
-                  />
-                  <Flex justifyContent="space-between" direction="column" width="15rem" >
-                    <Heading level={3}>{item.title}</Heading>
-                    <Text >
-                      {item.description}
-                    </Text>
-                  </Flex>
-                </Flex>
-              )
-
-            })}
-          </Flex>
-
-        </main>
-      </div>
-    </>
+    
+    <div className="Main">
+    <img className ="Image" src={brain} alt="brain image" onClick={getFact}></img>
+    {fact ?
+   <View
+      backgroundColor="white"
+      borderRadius="large"
+      fontSize="xxl"
+      textAlign="center"
+    >{fact}</View>
+: "Click on the brain to get some random fact"}
+    </div>
   );
 }
 
